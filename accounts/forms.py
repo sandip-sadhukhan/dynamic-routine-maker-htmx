030c1d7c6231e7a3da0de75feca6d35b21610563
Sandip Sadhukhan <sandippakhanna@gmail.com>
2024-05-05 07:36:47 +0530

    signup done

--- accounts/forms.py
+++ accounts/forms.py
@@ -17,3 +17,25 @@ class UserChangeForm(BaseUserChangeForm):
 class LoginForm(forms.Form):
     email = forms.EmailField()
     password = forms.CharField()
+
+
+class SignupForm(forms.Form):
+    email = forms.EmailField()
+    password = forms.CharField()
+
+    def clean_email(self):
+        email = self.cleaned_data["email"]
+
+        if User.objects.filter(email=email).exists():
+            self.add_error("email", "Email already exists")
+
+        return email
+    
+    def clean_password(self):
+        password = self.cleaned_data["password"]
+
+        if len(password) < 8:
+            self.add_error("password", "Password must be at least 8 characters long")
+
+        return password
+    
--- accounts/views.py
+++ accounts/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render
-from django.contrib.auth import authenticate, login as authLogin
-from accounts.forms import LoginForm
+from django.contrib.auth import authenticate, login as authLogin, logout as authLogout
+from accounts import forms
 from django.http import HttpResponse, Http404
 from django.views.decorators.http import require_POST
 from accounts.decorators import redirect_authenticated_user
 from django.urls import reverse
+from accounts.models import User
+from django.shortcuts import redirect
 
 
 @require_POST
 @redirect_authenticated_user
 def login(request):
-    form = LoginForm(request.POST)
+    form = forms.LoginForm(request.POST)
 
     if not form.is_valid():
         return render(request, "forms/login-form.html", {"loginForm": form})
@@ -27,3 +29,25 @@ def login(request):
     else:
         form.add_error("password", "Invalid credentials")
         return render(request, "forms/login-form.html", {"loginForm": form})
+
+
+@require_POST
+@redirect_authenticated_user
+def signup(request):
+    form = forms.SignupForm(request.POST)
+
+    if not form.is_valid():
+        return render(request, "forms/signup-form.html", {"signupForm": form})
+    
+    email = form.cleaned_data["email"]
+    password = form.cleaned_data["password"]
+
+    user = User.objects.create_user(email=email, password=password)
+    authLogin(request, user)
+    response = HttpResponse()
+    response["HX-Redirect"] = reverse("dashboard")
+    return response
+
+def logout(request):
+    authLogout(request)
+    return redirect("home")
