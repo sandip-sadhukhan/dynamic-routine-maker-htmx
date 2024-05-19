514268a4742ffb9244d6e585860d6748a75b3646
Nilesh Kumar <nileshkumar5896@gmail.com>
2024-05-19 16:44:06 +0530

    setup mysql database

--- core/settings.py
+++ core/settings.py
@@ -77,12 +77,24 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if config("DATABASE_ENGINE") == "django.db.backends.mysql":
+    DATABASES = {
+        'default': {
+            'ENGINE': config("DATABASE_ENGINE"),
+            'NAME': config("DATABASE_NAME"),
+            'USER': config("DATABASE_USER"),
+            'PASSWORD': config("DATABASE_PASSWORD"),
+            'HOST': config("DATABASE_HOST"),
+            'PORT': config("DATABASE_PORT"),
+        }
     }
-}
+elif config("DATABASE_ENGINE") == "django.db.backends.sqlite3":
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
